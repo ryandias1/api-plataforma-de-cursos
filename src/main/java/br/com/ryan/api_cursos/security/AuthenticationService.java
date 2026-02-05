204867f3a6fa42d54df8023197dc213d39bcb673
ryandias1 <ryan.cd@puccampinas.edu.br>
2026-02-05 10:37:49 -0300

    Criação de AuthenticationService para login

---
+++ src/main/java/br/com/ryan/api_cursos/security/AuthenticationService.java
@@ -0,0 +1,22 @@
+package br.com.ryan.api_cursos.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import br.com.ryan.api_cursos.repository.UserRepository;
+
+public class AuthenticationService implements UserDetailsService{
+
+    private final UserRepository userRepository;
+
+    public AuthenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado"));
+    }
+    
+}
