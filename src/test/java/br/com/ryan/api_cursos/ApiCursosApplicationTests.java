ed38c642b57f9532451b9df6c8cdf3acf4b0edf0
ryandias1 <ryan.cd@puccampinas.edu.br>
2026-02-05 08:37:49 -0300

    commit inicial

---
+++ src/test/java/br/com/ryan/api_cursos/ApiCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ryan.api_cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
